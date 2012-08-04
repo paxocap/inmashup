3933dcb31b4378eb19de351cc3dd160b18de064b
Clarence Leung <clarle@yahoo-inc.com>
2012-08-04 01:10:32 -0400

    Refactor code to use Y.App

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,38 @@
-YUI().use('transition', 'node-event-delegate', function (Y) {
-    var slides = ['intro', 'language', 'something', 'lol']
+YUI().use('app', 'transition', 'node-event-delegate', function (Y) {
+    var SlideshowApp;
 
-    function slideTransition (selectOne, selectTwo) {
-        var slideOne = Y.one('#' + selectOne + '-slide'),
-            slideTwo = Y.one('#' + selectTwo + '-slide');
+    SlideshowApp = Y.Base.create('slideshowApp', Y.App,  [], {
+        
+        slides: ['intro', 'language', 'something', 'lol'],
 
-        slideOne.transition({
-            easing: 'ease-out',
-            duration: 0.75,
-            left: '-1080px'
-        });
-        slideTwo.transition({
-            easing: 'ease-out',
-            duration: 0.75,
-            left: '218px'
-        });
-    };
+        initializer: function () {
+            this.setHandler(0);
+        },
 
-    function setHandler (index) {
-        Y.on('click', function () {
-            slideTransition(slides[index], slides[index + 1]);
-            setHandler(index + 1);
-        })
-    }
+        slideTransition: function (selectOne, selectTwo) {
+            var slideOne = Y.one('#' + selectOne + '-slide'),
+                slideTwo = Y.one('#' + selectTwo + '-slide');
 
-    setHandler(0);
+            slideOne.transition({
+                easing: 'ease-out',
+                duration: 0.75,
+                left: '-1080px'
+            });
+            slideTwo.transition({
+                easing: 'ease-out',
+                duration: 0.75,
+                left: '218px'
+            });
+        },
+
+        setHandler: function (index) {
+            var self = this;
+            Y.on('click', function () {
+                self.slideTransition(self.slides[index], self.slides[index+1]);
+                self.setHandler(index + 1);
+            });
+        }
+    });
+
+    new SlideshowApp();
 });
