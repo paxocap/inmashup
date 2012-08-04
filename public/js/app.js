59b638ae20261e74237d7018134ddad734cff91b
Clarence Leung <clarle@yahoo-inc.com>
2012-08-04 00:52:33 -0400

    Start slideshow interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-var stuff;
+YUI().use('transition', 'node-event-delegate', function (Y) {
+    var slides = ['intro', 'language', 'something', 'lol']
+
+    function slideTransition (selectOne, selectTwo) {
+        var slideOne = Y.one('#' + selectOne + '-slide'),
+            slideTwo = Y.one('#' + selectTwo + '-slide');
+
+        slideOne.transition({
+            easing: 'ease-out',
+            duration: 0.75,
+            left: '-1080px'
+        });
+        slideTwo.transition({
+            easing: 'ease-out',
+            duration: 0.75,
+            left: '218px'
+        });
+    };
+
+    function setHandler (index) {
+        Y.on('click', function () {
+            slideTransition(slides[index], slides[index + 1]);
+            setHandler(index + 1);
+        })
+    }
+
+    setHandler(0);
+});
